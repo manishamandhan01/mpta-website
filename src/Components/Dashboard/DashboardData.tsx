b4c7b5d8940af6ed5efbd724cd82306278665e5f
Shakti Singh Mandhan <shakti.mandhan@probo.in>
2025-05-21 08:58:57 +0530

    removed some tabs

--- src/Components/Dashboard/DashboardData.tsx
+++ src/Components/Dashboard/DashboardData.tsx
@@ -14,10 +14,10 @@ export const DashboardData = [
         icon:"fa-solid fa-table-cells-large",
         label:"Bank Transfer",
     },
-    {
-        icon:"fa-solid fa-address-card",
-        label:"Portfolio",
-    },
+    // {
+    //     icon:"fa-solid fa-address-card",
+    //     label:"Portfolio",
+    // },
     {
         icon:"fa-solid fa-chart-column",
         label:"Stock Position",
@@ -26,19 +26,19 @@ export const DashboardData = [
         icon:"fa-solid fa-hand-holding-dollar",
         label:"Dividends",
     },
-    {
-        icon:"fa-brands fa-searchengin",
-        label:"Trade Review",
-    },
+    // {
+    //     icon:"fa-brands fa-searchengin",
+    //     label:"Trade Review",
+    // },
 
     {
         icon:"fa-solid fa-trophy",
         label:"Calender",
     },
-    {
-        icon:"fa-solid fa-arrow-trend-up",
-        label:"Monthly Report",
-    },
+    // {
+    //     icon:"fa-solid fa-arrow-trend-up",
+    //     label:"Monthly Report",
+    // },
     // {
     //     icon:"fa-solid fa-upload",
     //     label:"Portfolio",
